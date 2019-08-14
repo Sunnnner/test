c83b61326afae2af936d75ab3425fe4f0752cdc1
zhangxu <zhangxu@knd.msn>
2019-08-14 15:42:59 +0800

    python demo

--- fly/app.py
+++ fly/app.py
@@ -16,8 +16,8 @@ app.layout = html.Div(children=[
         id = 'example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'pie', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'pie', 'name': u'Montreal'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montreal'},
             ],
             'layout': {
                 'title': 'Dash data'
---
+++ python_class/__init__demo.py
@@ -0,0 +1,7 @@
+class Fool(object):
+    def __init__(self, name='Mr.Li'):
+        self.name = name
+        print('my name is ', self.name)
+
+if __name__ == "__main__":
+    fool = Fool()
